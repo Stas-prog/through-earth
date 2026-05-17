5784e4261d13269cd04df5efb2c61e1a9b47e170
Stas-prog <skubav86@gmail.com>
2026-05-17 18:47:41 +0300

    fix next16 turbopack cesium

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {},
+
+  async headers() {
+    return [
+      {
+        source: "/cesium/:path*",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
